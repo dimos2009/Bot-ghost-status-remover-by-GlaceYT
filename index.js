428f74545538f45b1bc5f2f52aaae282d87b0229
dimos2009 <dimostenismpampalis2@gmail.com>
2024-08-25 00:41:07 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Playing Diamond Roleplay","Listening Diamond Roleplay"];
 
 
 let currentIndex = 0;
